f9c9e9ce4c9f075e888981fcbd43957d750d7cee
LNasya Syafitrie <lnasya.syafitrie@gmail.com>
2024-05-04 08:24:39 +0700

    done

--- src/test/java/starter/mentutor/MentutorAPIAdmin.java
+++ src/test/java/starter/mentutor/MentutorAPIAdmin.java
@@ -13,8 +13,23 @@ public class MentutorAPIAdmin {
 
     public static String REGISTER_NEW_USERS_ADMIN = Constants.BASE_URL + "admin/users";
 
-//    ADMIN
+    public static String GET_ALL_USERS_ADMIN = Constants.BASE_URL + "admin/users";
 
+    public static String REGISTER_NEW_CLASS_ADMIN = Constants.BASE_URL + "admin/classes";
+
+    public static String GET_ALL_CLASS_ADMIN = Constants.BASE_URL + "admin/classes";
+
+    public static String UPDATE_USER_ADMIN = Constants.BASE_URL + "admin/users/{id_user}";
+
+    public static String GET_USER_ADMIN = Constants.BASE_URL + "admin/users/{id_user}";
+
+    public static String UPDATE_CLASS_ADMIN = Constants.BASE_URL + "admin/classes/{id_class}";
+
+    public static String DELETE_USER_ADMIN = Constants.BASE_URL + "admin/users/{id_user}";
+
+    public static String DELETE_CLASS_ADMIN = Constants.BASE_URL + "admin/classes/{id_class}";
+
+//    ADMIN
     @Step("Login as admin")
     public void loginAsAdmin(File json) {
         SerenityRest.given()
@@ -28,5 +43,97 @@ public class MentutorAPIAdmin {
                 .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
                 .contentType(ContentType.JSON)
                 .body(json);
+    }
+
+    @Step("Get all users as admin")
+    public void setGetAllUsersAdmin() {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN);
+    }
+
+    @Step("Register new class as admin")
+    public void registerNewClassAsAdmin(File json) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    @Step("Get all class as admin")
+    public void setGetAllClassAdmin() {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN);
+    }
+
+    @Step("Update user as admin")
+    public void setUpdateUserAdmin(File json, int id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    @Step("Get user as admin")
+    public void setGetUserAdmin(int id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user);
+    }
+
+    @Step("Update class as admin")
+    public void setUpdateClassAdmin(File json, int id_class) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_class", id_class)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    @Step("Update user as admin")
+    public void setUpdateUserInvalidAdmin(File json, String id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    @Step("Get user invalid as admin")
+    public void setGetUserInvalidAdmin(String id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user);
+    }
+
+    @Step("Update class invalid path as admin")
+    public void setUpdateClassInvalidPathAdmin(File json, String id_class) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_class", id_class)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+    @Step("Update user invalid json as admin")
+    public void setUpdateUserInvalidJsonAdmin(File json, int id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user)
+                .contentType(ContentType.JSON)
+                .body(json);
+    }
+
+    @Step("Delete user as admin")
+    public void setDeleteUserAdmin(int id_user) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_user", id_user);
+    }
+
+    @Step("Delete class as admin")
+    public void setDeleteClassAdmin(int id_class) {
+        SerenityRest.given()
+                .headers("Authorization", MentutorResponsesAdmin.ADMIN_TOKEN)
+                .pathParam("id_class", id_class);
     }
 }
--- src/test/java/starter/mentutor/MentutorResponsesAdmin.java
+++ src/test/java/starter/mentutor/MentutorResponsesAdmin.java
@@ -2,9 +2,32 @@ package starter.mentutor;
 
 public class MentutorResponsesAdmin {
 
-    public static String ADMIN_TOKEN_1 = "";
+//    public static String ADMIN_TOKEN_1 = "";
 
-    public static String ADMIN_TOKEN = "Bearer " + ADMIN_TOKEN_1 ;
+//    public static String ADMIN_TOKEN = "Bearer " + ADMIN_TOKEN_1 ;
+
+    public static String ADMIN_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZENsYXNzIjowLCJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MTQ3MTY1NjIsInJvbGUiOiJhZG1pbiIsInVzZXJJZCI6MX0.AhO4uz6g4BaYBzmvEYKkZI0l07AuWX08dnxbhxW4D7Q" ;
 
     public static String USER_NAME_ADMIN = "data.name";
+
+    public static String ID_ALL_USER_ADMIN = "data[0].id_user";
+
+    public static String REGISTER_CLASS_SUCCESS_MESSAGE_ADMIN = "message";
+
+    public static String ID_ALL_CLASS_ADMIN = "data[0].id_class";
+
+    public static String UPDATE_USER_SUCCESS_MESSAGE_ADMIN = "message";
+
+    public static String USER_NAME_UPDATE_SUCCESS = "data.name";
+
+    public static String GET_USER_SUCCESS_MESSAGE_ADMIN = "message";
+
+    public static String USER_NAME_GET_SUCCESS = "data.name";
+
+    public static String UPDATE_CLASS_SUCCESS_MESSAGE_ADMIN = "message";
+
+    public static String CLASS_NAME_UPDATE_SUCCESS = "data.class_name";
+
+    public static String INVALID_MESSAGE_ADMIN = "message error";
+
 }
